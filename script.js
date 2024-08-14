0060feaaf820283ec4c139dafdf49b7933918929
Léo/L-Dev <86838693+L-Dev31@users.noreply.github.com>
2024-08-14 16:45:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (window.innerWidth < 768) {  
         speed = 50;
     } else {
-        speed = 500;
+        speed = 200;
     }
 
     scrollingTexts.forEach(p => {
