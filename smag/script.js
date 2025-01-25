0c6513c09aa314be501c866540a4977aaea8bacb
Léo/L-Dev <86838693+L-Dev31@users.noreply.github.com>
2025-01-25 19:03:21 -0400

    UPD

--- smag/script.js
+++ smag/script.js
@@ -1,4 +1,18 @@
-//Screenshots Carousel
+// Music
+const audioElements = document.querySelectorAll('audio');
+
+audioElements.forEach(audio => {
+    audio.addEventListener('play', () => {
+        audioElements.forEach(otherAudio => {
+            if (otherAudio !== audio && !otherAudio.paused) {
+                otherAudio.pause();
+                otherAudio.currentTime = 0; 
+            }
+        });
+    });
+});
+
+// Screenshots Carousel
 const picCtn = document.querySelector('.pic-ctn');
 const pics = Array.from(picCtn.querySelectorAll('.pic'));
 const prevBtn = document.querySelector('.prev-btn');
@@ -8,10 +22,7 @@ let currentIndex = 0;
 let interval;
 
 function showImage(index) {
-  // Masquer toutes les images
   pics.forEach((pic) => pic.classList.remove('active'));
-
-  // Afficher l'image actuelle
   pics[index].classList.add('active');
 }
 
@@ -33,7 +44,6 @@ function stopCarousel() {
   clearInterval(interval);
 }
 
-// Boutons de navigation
 prevBtn.addEventListener('click', () => {
   stopCarousel();
   prevImage();
@@ -46,25 +56,7 @@ nextBtn.addEventListener('click', () => {
   startCarousel();
 });
 
-// Pause au survol
 picCtn.addEventListener('mouseenter', stopCarousel);
 picCtn.addEventListener('mouseleave', startCarousel);
 
-// Démarrer le carrousel
 startCarousel();
-
-
-// Music
-const audioElements = document.querySelectorAll('audio');
-
-audioElements.forEach(audio => {
-    audio.addEventListener('play', () => {
-        // Stop every songs except the one that is playing
-        audioElements.forEach(otherAudio => {
-            if (otherAudio !== audio && !otherAudio.paused) {
-                otherAudio.pause();
-                otherAudio.currentTime = 0; // Reset Music Timer to 0
-            }
-        });
-    });
-});
